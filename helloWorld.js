1a4f6466c485b78a99adffcc0e8b675123d10399
Jan Van Lysebeth <jan.van.lysebeth@mediagenix.tv>
2017-11-04 13:09:37 +0100

    initial

---
+++ helloWorld.js
@@ -0,0 +1,8 @@
+const http = require('http'); 
+http.createServer(function(req, resp) {
+    resp.writeHead(200, {'Content-Type': 'text/html'});
+    resp.write('<h1>Het heizel drama</h1>');
+    resp.write('<img src="http://www.rizoomes.nl/wp-content/uploads/2014/05/Heizeldrama-verdrukking.jpg"></img>')
+    resp.write('<h2>Aanloop</h2>');
+    resp.end('<p>Het was een zaterdag in mei 1985.</p>');
+}).listen(1337, '127.0.0.1');
